692dcb720f9b62ce2f84d162391c68871dc700ca
weemizo <mrceleius@gmail.com>
2023-06-28 15:22:29 +0200

    feat(FetchWrapper): just added a FetchWrapper.

--- CV_website/src/main.jsx
+++ CV_website/src/main.jsx
@@ -8,3 +8,20 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+export default class FetchWrapper {
+  constructor(baseURL) {
+    this.baseURL = baseURL
+  }
+
+  get(endpoint) {
+    return fetch(this.baseURL + endpoint, {
+      method: "get", // ikr its a default
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => response.json())
+
+  }
+}
