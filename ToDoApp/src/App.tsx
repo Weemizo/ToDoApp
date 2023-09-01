92f40ea49d745c073ee48ee54fedc61111269b8f
weemizo <mrceleius@gmail.com>
2023-09-01 19:05:25 +0200

    now its time to add the ToDoApp to the git repo

---
+++ ToDoApp/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <main>
+          <div className="header">
+
+            Basic To Do App
+
+          </div>
+
+          <div className="">
+      
+          </div>
+        </main>
+    </>
+  )
+}
+
+export default App
