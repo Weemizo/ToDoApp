9b232b809a6773d710ee747a98a7bf4f77eeb53f
weemizo <mrceleius@gmail.com>
2023-09-10 19:20:00 +0200

    keeping a good habit. need to figure out how the app will look like

--- ToDoApp/src/App.tsx
+++ ToDoApp/src/App.tsx
@@ -1,20 +1,19 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
   return (
     <>
-        <main>
+        <div className="main">
           <div className="header">
-
             Basic To Do App
-
           </div>
 
-          <div className="">
-      
+          <div className='container'>
+            <input type="text" className='addToDo' value="Type your To-Do here"/> 
           </div>
-        </main>
+        </div>
     </>
   )
 }
